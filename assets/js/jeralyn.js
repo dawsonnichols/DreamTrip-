adc5f3f82ead0b384c48c861b2a479ee2966b9b7
jbairoso <jbairoso@gmail.com>
2022-05-02 19:04:17 -0700

    fetch current location

---
+++ assets/js/jeralyn.js
@@ -0,0 +1,9 @@
+fetch(
+    'https://ipgeolocation.abstractapi.com/v1/?api_key=dd4801d4daad4dd9b8407e3557ee14d7'
+)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    });
