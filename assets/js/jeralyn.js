5e93a02d9e403936ffd9eeba1be9c0729f0ffee8
jbairoso <jbairoso@gmail.com>
2022-05-02 20:15:24 -0700

    display city name on site

--- assets/js/jeralyn.js
+++ assets/js/jeralyn.js
@@ -1,9 +1,27 @@
+var geoContainer=document.getElementById('city');
+
 fetch(
-    'https://ipgeolocation.abstractapi.com/v1/?api_key=dd4801d4daad4dd9b8407e3557ee14d7'
+    "https://ipgeolocation.abstractapi.com/v1/?api_key=dd4801d4daad4dd9b8407e3557ee14d7"
 )
     .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
+         return response.json();
+        })
+        .then(function(data){
         console.log(data);
-    });
+
+        var cityName=document.getElementById('displayCity');
+
+        cityName.textContent = data.city;        
+});
+
+
+
+
+
+
+
+
+
+
+
+
