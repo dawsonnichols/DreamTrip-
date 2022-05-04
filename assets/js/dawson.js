9be7600b0675e5582625026e3ea879435079d93a
dawsonnichols <85593807+dawsonnichols@users.noreply.github.com>
2022-05-04 12:34:24 -0700

    for loop

--- assets/js/dawson.js
+++ assets/js/dawson.js
@@ -1,55 +1,67 @@
-function restaurants() {
-const encodedParams = new URLSearchParams();
-encodedParams.append("q", );
-encodedParams.append("language", "en_US");
-let cityCode = 0;
-const options = {
-  method: "POST",
-  headers: {
-    "content-type": "application/x-www-form-urlencoded",
-    "X-RapidAPI-Host": "worldwide-restaurants.p.rapidapi.com",
-    "X-RapidAPI-Key": "a9ead3f843msh2212025a98c2772p142d07jsn6a69bddd4809",
-  },
-  body: encodedParams,
-};
+let restaurantImageDisplay1 = document.getElementById("image1");
+const img = document.createElement("img");
+var imgSrcOne = document.getElementById("imageOne");
+var imgSrcTwo = document.getElementById("imageOne");
+var imgSrcThree = document.getElementById("imageOne");
+var imgSrcFour = document.getElementById("imageOne");
+var restaurantsDisplay = $("#restaurants");
+function restaurants(cityName) {
+  const encodedParams = new URLSearchParams();
+  encodedParams.append("q", cityName);
+  encodedParams.append("language", "en_US");
+  let cityCode = 0;
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "X-RapidAPI-Host": "worldwide-restaurants.p.rapidapi.com",
+      "X-RapidAPI-Key": "a9ead3f843msh2212025a98c2772p142d07jsn6a69bddd4809",
+    },
+    body: encodedParams,
+  };
 
-fetch("https://worldwide-restaurants.p.rapidapi.com/typeahead", options)
-  .then((response) => response.json())
-  .then((response) => {
-    cityCode = response.data[0].result_object.location_id;
-    console.log(response);
-    //   .catch((err) => console.error(err));
-  });
-const encodedParams1 = new URLSearchParams();
-encodedParams1.append("language", "en_US");
-encodedParams1.append("limit", "4");
-encodedParams1.append("location_id", + cityCode);
-encodedParams1.append("currency", "USD");
+  fetch("https://worldwide-restaurants.p.rapidapi.com/typeahead", options)
+    .then((response) => response.json())
+    .then((results) => {
+      cityCode = results.results.data[0].result_object.location_id;
+      console.log(results);
+      console.log(cityCode);
+      //   .catch((err) => console.error(err));
+      const encodedParams1 = new URLSearchParams();
+      encodedParams1.append("language", "en_US");
+      encodedParams1.append("limit", "4");
+      encodedParams1.append("location_id", cityCode);
+      encodedParams1.append("currency", "USD");
 
-const options1 = {
-  method: "POST",
-  headers: {
-    "content-type": "application/x-www-form-urlencoded",
-    "X-RapidAPI-Host": "worldwide-restaurants.p.rapidapi.com",
-    "X-RapidAPI-Key": "a9ead3f843msh2212025a98c2772p142d07jsn6a69bddd4809",
-  },
-  body: encodedParams1,
-};
+      const options1 = {
+        method: "POST",
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          "X-RapidAPI-Host": "worldwide-restaurants.p.rapidapi.com",
+          "X-RapidAPI-Key":
+            "a9ead3f843msh2212025a98c2772p142d07jsn6a69bddd4809",
+        },
+        body: encodedParams1,
+      };
 
-fetch("https://worldwide-restaurants.p.rapidapi.com/search", options1)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+      fetch("https://worldwide-restaurants.p.rapidapi.com/search", options1)
+        .then((response) => response.json())
+        .then((data) => {
+          restaurantFunc(data);
+
+          console.log(data).catch((err) => console.error(err));
+        });
+    });
 }
-// //first.js
-// const colorCode = {
-//   black: "#000",
-//   white: "#fff"
-// };
-// export { colorCode };
-// Then, import the variable in second file using import.
+var restaurantFunc = function (data) {
+  
+  for (var i = 0; i <= 3; i++) {
+    var restaurantImage = $(
+      "<img src=" + data.results.data[i].photo.images.small.url + ">"
+    );
+    var restaurantCard = $("<div>").addClass("col s3");
+  }
 
-// //second.js
-// import { colorCode } from './first.js'
-
-// get variables across .js files if its in global scope, careful of "race condition". 
+  restaurantsDisplay.append(restaurantCard);
+  restaurantCard.append(restaurantImage);
+};
